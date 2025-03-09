6056ab8cc43ef90a205c5b2591822f2e9704f21f
Gearóid O'Ceallacháin <gocealla@gmail.com>
2025-03-09 20:45:19 +0200

    created a Netlify folder and fetchData.js file

---
+++ src/netlify/functions/fetchData.js
@@ -0,0 +1,21 @@
+export.handler = async (event) => {
+    const { playlistId, uris, accessToken } = JSON.parse(event.body);
+    const tracks = await fetch(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+        method: 'POST',
+        headers: {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+        uris: uris,
+        }),
+    })
+        .then(response => response.json())
+        .then(() => {
+        return Promise.resolve();
+        });
+    return {
+        statusCode: 200,
+        body: JSON.stringify(tracks),
+    };
+    }
